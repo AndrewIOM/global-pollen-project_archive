b8b449c9fddc7e8642fd06441578820ef53ca15c
Andrew Martin <andrew.martin@acm.im>
2015-11-22 11:21:02 +0000

    Missed files in last commit

---
+++ src/OxPollen/Models/Grain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OxPollen.Models
+{
+    public class Grain
+    {
+        [Key]
+        public int GrainId { get; set; }
+
+        [Required]
+        public virtual List<GrainImage> Images { get; set; }
+        [Required]
+        public virtual AppUser SubmittedBy { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public DateTime TimeAdded { get; set; }
+
+        //Optional
+        public int? AgeYearsBeforePresent { get; set; }
+
+        //Identifications
+        public virtual List<Identification> Identifications { get; set; }
+        public virtual Taxon Taxon { get; set; }
+    }
+}
