7b08b9f2fd8ed37b6f400209702e23944420d3f1
Andrew Martin <andrew.martin@acm.im>
2016-01-14 14:34:32 +0000

    Updated image grid to fill images

--- src/OxPollen/wwwroot/js/site.js
+++ src/OxPollen/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿// Write your Javascript code.
+﻿//Image Fill
+$(".img-container").each(function () {
+    var refRatio = 300 / 300;
+    var imgH = $(this).children("img").height();
+    var imgW = $(this).children("img").width();
+    if ((imgW / imgH) < refRatio) {
+        $(this).addClass("portrait");
+    } else {
+        $(this).addClass("landscape");
+    }
+})
