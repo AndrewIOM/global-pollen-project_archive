d99ccce371ec26ef8865baff895e4cb8d18a160d
Andrew Martin <andrew.martin@acm.im>
2016-01-18 16:30:35 +0000

    Feature: Neotoma data on taxon page

--- src/OxPollen/wwwroot/js/site.js
+++ src/OxPollen/wwwroot/js/site.js
@@ -1,11 +1,13 @@
 ﻿//Image Fill
-$(".img-container").each(function () {
-    var refRatio = 300 / 300;
-    var imgH = $(this).children("img").height();
-    var imgW = $(this).children("img").width();
-    if ((imgW / imgH) < refRatio) {
-        $(this).addClass("portrait");
-    } else {
-        $(this).addClass("landscape");
-    }
-})
+$(document).ready(function () {
+    $(".img-container").each(function () {
+        var refRatio = 300 / 300;
+        var imgH = $(this).children("img").height();
+        var imgW = $(this).children("img").width();
+        if ((imgW / imgH) < refRatio) {
+            $(this).addClass("portrait");
+        } else {
+            $(this).addClass("landscape");
+        }
+    })
+});
